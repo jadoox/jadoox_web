fcc1cea42acd0feb5145a8e36a73b07e463413f6
unknown <martin.sohl@hotmail.com>
2022-06-04 00:33:23 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,14 @@
+function backgroundChange() {
+
+    var button = document.getElementById("1");
+
+    if(document.body.style.backgroundImage == 'url("/wallhaven-k93l37.jpg")') {
+        document.body.style.backgroundImage = '';
+        button.innerText = 'Boring background?'
+    }
+    else {
+        document.body.style.transition = 'all 1s ease-in-out';
+        document.body.style.backgroundImage = 'url("/wallhaven-k93l37.jpg")'
+        button.innerText = 'Better?'
+    }
+}
