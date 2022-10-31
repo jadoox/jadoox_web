9dc7803160475a12daeb6f998d89c909f970aa7d
Martin Sohl <martin.sohl@hotmail.com>
2022-10-31 16:48:58 +0100

    Formatted code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function backgroundChange() {
 
     var button = document.getElementById("1");
 
-    if(document.body.style.backgroundImage == 'url("/wallhaven-k93l37.jpg")') {
+    if (document.body.style.backgroundImage == 'url("/wallhaven-k93l37.jpg")') {
         document.body.style.backgroundImage = '';
         button.innerText = 'Boring background?'
     }
